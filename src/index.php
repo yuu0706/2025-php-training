5affe7b98458b1d867a0cc3ab0fa052ee6558b5b
yuuyamaguchi <yuu.pute.0004@gmail.com>
2024-10-18 14:26:01 +0900

    表作成

--- src/index.php
+++ src/index.php
@@ -5,5 +5,17 @@
     </head>
     <body>
         <?php echo '<p>Hello World</p>'; ?>
+        <button onclick=header(https://www.google.co.jp/)>追加</button>
+        <table>
+            <tr>
+                <th>番号</th>
+                <th>タイトル</th>
+                <th>内容</th>
+                <th>作成日</th>
+                <th>更新日</th>
+                <th> </th>
+            </tr>
+            <tr></tr>
+        </table>
     </body>
 </html>
